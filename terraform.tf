2da48a68c7756f968af9cd9f13453fefffa15346
dasari suresh <sureshdasari799@gmail.com>
2024-08-05 11:57:57 -0400

    updated provider file

--- terraform.tf
+++ terraform.tf
@@ -1,1 +1,15 @@
 # providers
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.61.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+  profile = "default"
+}
